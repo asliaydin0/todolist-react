5dcc718e717534e5cf4669f9be21808a8d4ab08d
Aslı AYDIN <asliaydn12204@gmail.com>
2026-07-19 17:44:33 +0300

    hata çözüldü

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import { TodoForm } from './TodoForm'
 import { Todo } from './Todo';
 import { v4 as uuidv4 } from 'uuid';
+import { EditTodoForm } from './EditTodoForm';
 uuidv4();
 
 export const TodoWrapper = () => {
@@ -27,9 +28,10 @@ export const TodoWrapper = () => {
         <TodoForm addTodo={addTodo} />
         {todos.map((todo, index) => (
             todo.isEditing ? (
-                <EditTodoForm/>) : (
-          <Todo task={todo} key={index} 
-          toggleComplete={toggleComplete} deleteTodo={deleteTodo} editTodo={editTodo}/>
+                <EditTodoForm />
+              ) : (
+                  <Todo task={todo} key={index} 
+                  toggleComplete={toggleComplete} deleteTodo={deleteTodo} editTodo={editTodo} />
          )
         ))}
     </div>
